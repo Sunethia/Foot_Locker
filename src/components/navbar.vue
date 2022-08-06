d3926f1aa2d1b2f2ec4e4dec31d0465551368a19
Sunethia <lawrencesunethia@gmail.com>
2022-08-06 14:48:25 +0200

    first commit

---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <ul>
+        <li><a href="/home">Home</a></li>
+        <li><a href="">Products</a></li>
+        <li><a href="">Sign Up</a></li>
+        <li><a href="">Login</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: white;
+  border-bottom: 2px solid black;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-family: "Rampart One", cursive;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+  background-color: grey;
+}</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <div class="container">
+      <div class="row">
+        <h1>FOOT LOCKER</h1>
+      </div>
+    </div>
+    <div class="row">
+      <img src="../assets/home.jpg" alt="home" />
+    </div>
+  </div>
+</template>
+<script>
+import Home from "../components/Home.vue";
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
+<style scoped>
+h1 {
+  font-size: 10rem;
+  font-family: "Bree Serif", serif;
+}
+img {
+  width:50rem;
+}
+</style>
